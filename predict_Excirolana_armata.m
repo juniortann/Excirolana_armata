334ba45ec50310895a5e37e34d7843a3a6976ee3
juniortann <juniortann@gmail.com>
2021-05-20 13:05:31 -0300

    Change DEB
    Change DEB

--- predict_Excirolana_armata.m
+++ predict_Excirolana_armata.m
@@ -6,7 +6,7 @@ function [prdData, info] = predict_Excirolana_armata(par, data, auxData)
   
   %% compute temperature correction factors
   TC = tempcorr(temp.am, T_ref, T_A); kT_M = k_M * TC;
-
+  
   %% zero-variate data
 
   % life cycle
@@ -26,14 +26,14 @@ function [prdData, info] = predict_Excirolana_armata(par, data, auxData)
   aT_p = t_p/ kT_M;                 % d, age at puberty at f and T
 
   % ultimate
-  l_i = f - l_T;                    % -, scaled ultimate length at f
+  l_i = f_Li - l_T;                    % -, scaled ultimate length at f
   L_i = L_m * l_i;                  % cm, ultimate structural length at f
   Lw_i = L_i/ del_M;                % cm, ultimate physical length at f
-  Ww_i = L_i^3 * (1 + f * w);       % g, ultimate wet weight (remove d_V for wet weight)
+  Ww_i = L_i^3 * (1 + f_Li * w);       % g, ultimate wet weight (remove d_V for wet weight)
  
   % reproduction
   pars_R = [kap; kap_R; g; k_J; k_M; L_T; v; U_Hb; U_Hp]; % compose parameter vector at T
-  RT_i = TC * reprod_rate((100 * del_M), f, pars_R);       % #/d, ultimate reproduction rate at T
+  RT_i = TC * reprod_rate(L_i, f, pars_R);             % #/d, ultimate reproduction rate at T
 
   % life span
   pars_tm = [g; l_T; h_a/ k_M^2; s_G];  % compose parameter vector at T_ref
@@ -48,18 +48,17 @@ function [prdData, info] = predict_Excirolana_armata(par, data, auxData)
   prdData.Lb = Lw_b;
   prdData.Lp = Lw_p;
   prdData.Li = Lw_i;
-%   prdData.Wwb = Ww_b;
-%   prdData.Wwp = Ww_p;
-%   prdData.Wwi = Ww_i;
+  prdData.Wwb = Ww_b;
+  prdData.Wwp = Ww_p;
+  prdData.Wwi = Ww_i;
   prdData.Ri = RT_i;
   
   %% uni-variate data
   % time-length
-  ir_B = 3/ k_M + 3 * f * L_m/ v; r_B = 1/ ir_B; % d, 1/von Bert growth rate
-  Lw_i = (f * L_m - L_T)/ del_M; % ultimate physical length at f
-  pars_lb = [g k v_Hb];
-  Lw_b = get_lb(pars_lb, f) * L_m/ del_M; % -, physical length at birth at f
-  ELw = Lw_i - (Lw_i - Lw_b) * exp( - TC * r_B * tL(:,1)); % cm, expected physical length at time
+  [t_p, t_b, l_p, l_b] = get_tp(pars_tp, f_tL);
+  r_B = k_M/ 3/ (1 + f_tL/ g);  L_i = L_m * f_tL; L_b = L_m * l_b;      
+  [t L] = ode45(@get_L, tL(:,1), L_b, [], r_B, L_i, T_ref, T_A); % cm, structural length 
+  ELw = L/ del_M;                       % g, total length
   
   % L-N data
   EN = TC * reprod_rate (LN(:,1) * del_M, f, pars_R) * 365/ 6;  % #/d, clutch size for spawning six times per yr (every two months)
@@ -68,3 +67,8 @@ function [prdData, info] = predict_Excirolana_armata(par, data, auxData)
   prdData.tL = ELw;  
   prdData.LN = EN;
   
+end
+function dL = get_L(t, L, r_B, L_i, T_ref, T_A)
+  TC = tempcorr(C2K(27+7*sin(2*pi*(t+10)/365)), T_ref, T_A); 
+  dL = TC * r_B * (L_i - L);
+end
